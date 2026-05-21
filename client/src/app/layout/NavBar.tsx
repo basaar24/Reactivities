26114371df6d1f32c335e3662a4c3f8300a517fc
Arturo Barajas <basaar.24@gmail.com>
2026-05-21 11:57:45 -0600

    client - Add basic form to craete/edit an activity

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -8,7 +8,11 @@ import {
   Typography,
 } from "@mui/material";
 
-export default function NavBar() {
+type Props = {
+  openForm: () => void;
+}
+
+export default function NavBar({openForm}: Props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -58,7 +62,7 @@ export default function NavBar() {
                 Contact
               </Button>
             </Box>
-            <Button size="large" variant="contained" color="warning">
+            <Button onClick={openForm} size="large" variant="contained" color="warning">
               Create Activity
             </Button>
           </Toolbar>
